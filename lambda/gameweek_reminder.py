2fb6446378409a14705561ede1f4ae71b7c6d90a
phakawatTER <phakawatTER@github.com>
2023-12-28 15:12:29 +0700

    gameweek reminder update gameweek in advance

--- lambda/gameweek_reminder.py
+++ lambda/gameweek_reminder.py
@@ -16,38 +16,49 @@ LINE_UP_STATEMACHINE_ARN = "arn:aws:states:ap-southeast-1:661296166047:stateMach
 async def execute():
     app = App()
     gw_status = await app.fpl_service.get_current_gameweek()
-    current_gameweek = gw_status.event
-    current_gameweek_fixtures = await app.fpl_service.list_gameweek_fixtures(
-        gameweek=current_gameweek
+    fpl_current_gameweek = gw_status.event + 1
+    system_current_gameweek = app.fpl_service.get_current_gameweek_from_dynamodb()
+
+    if system_current_gameweek >= fpl_current_gameweek:
+        return
+
+    next_gameweek = system_current_gameweek + 1
+    # end of season do nothing
+    if next_gameweek > 38:
+        return
+
+    next_gameweek_fixtures = await app.fpl_service.list_gameweek_fixtures(
+        gameweek=next_gameweek
     )
     earliest_match: FPLMatchFixture = None
-    for fixture in current_gameweek_fixtures:
+    for fixture in next_gameweek_fixtures:
         if earliest_match is None:
             earliest_match = fixture
         elif fixture.kickoff_time < earliest_match.kickoff_time:
             earliest_match = fixture
+
     time_to_subtract = timedelta(
-        hours=8
+        hours=16
     )  # should remind 8 hours before the first match of gameweek
     time_to_remind = earliest_match.kickoff_time - time_to_subtract
     now = datetime.utcnow().astimezone()
-    should_remind = now >= time_to_remind
-    latest_gameweek = app.fpl_service.get_current_gameweek_from_dynamodb()
-    if current_gameweek != latest_gameweek and should_remind:
+    should_update_gameweek = now >= time_to_remind
+
+    if should_update_gameweek:
         line_group_ids = app.firebase_repo.list_line_channels()
         for group_id in line_group_ids:
             app.message_service.send_gameweek_reminder_message(
-                gameweek=current_gameweek,
+                gameweek=next_gameweek,
                 group_id=group_id,
             )
-        app.fpl_service.update_gameweek(gameweek=current_gameweek)
+        app.fpl_service.update_gameweek(gameweek=next_gameweek)
         # notify line-up flex message when the first match of gameweek is played
         time_to_remind = earliest_match.kickoff_time
         time_to_remind = time_to_remind.replace(microsecond=0)
         app.sfn.start_execution(
             state_machine_arn=LINE_UP_STATEMACHINE_ARN,
             input_data={
-                "gameweek": current_gameweek,
+                "gameweek": next_gameweek,
                 "timeToNotify": time_to_remind.isoformat(),
             },
         )
