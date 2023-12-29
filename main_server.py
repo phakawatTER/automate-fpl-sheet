b3d15af1091b096c8153f73b2304154d1e9b67f3
phakawatTER <phakawatTER@github.com>
2023-12-29 17:14:30 +0700

    fix dependencies too large when deploy

--- main_server.py
+++ main_server.py
@@ -1,8 +1,13 @@
 from api import LineMessageAPI
 from app import App
 
-if __name__ == "__main__":
+
+def main() -> None:
     fpl_app = App()
     line_message_api = LineMessageAPI(app=fpl_app)
     app = line_message_api.initialize()
     app.run(port=5100, debug=True)
+
+
+if __name__ == "__main__":
+    main()
